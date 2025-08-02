e59e7576b5398eb2644fdfffb07bac13e2754978
Ndubueze Eke <ndubuezeeke@gmail.com>
2025-08-02 15:07:36 +0100

    shell script added

---
+++ nodeHealth.sh
@@ -0,0 +1,26 @@
+
+#!/bin/bash
+
+
+########################
+
+
+# Author: Ndubueze Eke
+# Date:   July 31,2025
+#
+# Tjis script outputs the node health
+#
+# Version 1
+
+
+########################
+
+set -x   # debug mode
+
+df -h
+
+
+free -g
+
+
+nproc
